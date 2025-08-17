a3d3412cec2ecbd8de59e68612ebb6b418fd9380
Vnntnn <thanabodee.nine@gmail.com>
2025-08-18 01:22:53 +0700

    2 timeout testcases left.

---
+++ PhyCom_Lab/Midterm/Free/Spotify_Easy.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  long int min, queues;
+  int sec;
+
+  scanf("%ld.%d", &min, &sec);
+  scanf("%ld", &queues);
+
+  int listen_time = sec + (min * 60);
+
+  char queue_song[queues][51], song_name[51];
+  long int song_duration[queues], total_play_time = 0;
+
+  for (int i = 0; i < queues; i++) {
+    int song_duration_min, song_duration_sec, queue_num;
+    scanf(" Queue#%d <|> %[^<] <|> %d.%d", &queue_num, song_name, &song_duration_min, &song_duration_sec);
+    song_duration[queue_num - 1] = song_duration_sec + (song_duration_min * 60);
+    total_play_time += song_duration[queue_num - 1];
+    strcpy(queue_song[queue_num - 1], song_name);
+  }
+
+  if (listen_time > total_play_time) listen_time %= total_play_time;
+  int last_queue_listen;
+  int idx = 0;
+
+  if (listen_time == 0) {
+    idx = queues - 1;
+    last_queue_listen = 100;
+  } else {
+    while (idx < queues && listen_time > song_duration[idx]) {
+      listen_time -= song_duration[idx];
+      idx++;
+    }
+    last_queue_listen = (listen_time * 100) / song_duration[idx];
+  }
+
+  int final_queue = (last_queue_listen == 0 && listen_time > 0) ? 1 : last_queue_listen;
+
+  printf("Song Order: %d\nSong Name: %s\n", idx + 1, queue_song[idx]);
+  if (final_queue == 100) printf("Song Process: Complete"); 
+  else printf("Song Process: %d%%", final_queue);
+  printf("\n");
+
+  return 0;
+}
